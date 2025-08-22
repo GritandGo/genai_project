11401ccd3841be6000d8ac1c139999870d7d85a0
GritandGo <codemain0123@gmail.com>
2025-08-22 13:14:55 -0500

    feat: add system prompt configuration for AI model responses

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from google.genai.types import GenerateContentConfig
 
 
 load_dotenv()
@@ -30,9 +31,12 @@ def main():
     ]
 
 
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
         
     prompt_tokens = response.usage_metadata.prompt_token_count
